47fa6d21f4183c4de6f0b5cd2c31158c961f5b74
Casey Cooper <caseykcooper14@gmail.com>
2020-09-04 16:34:36 -0700

    Fix get_protein_facts function to better accomadate more proteins

--- basic_protein_information.py
+++ basic_protein_information.py
@@ -7,7 +7,16 @@ def get_protein_facts (name):
     response = requests.get(base_url + name).text
     stripped = re.sub('<[^<]+?>', '', response)
     start_ind = stripped.find("Kids Encyclopedia Facts")
-    facts = stripped[start_ind:stripped.find("Related pages", start_ind)]
+    loop_ind = stripped.find("Kids Encyclopedia Facts")
+    img_count = stripped.count("Images for kids")
+    for i in range(1, img_count):
+        img_ind = stripped.find("Images for kids", loop_ind)
+        if i != img_count:
+            loop_ind = img_ind + 15
+    facts = stripped[start_ind:stripped.find("Images for kids", loop_ind)]
+    facts = facts[0:facts.find("Related pages", 0)]
     return facts;
 
-print(get_protein_facts('lysozyme'))
+print(get_protein_facts('myosin'))
+
+#need to loop through count of images and set index to after the second to last image
