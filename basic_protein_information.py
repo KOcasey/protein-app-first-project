afe5f5e291c99ac9fa70bd37a5207705155c7dec
Casey Cooper <caseykcooper14@gmail.com>
2020-09-06 20:08:14 -0700

    Remove google ads from the protein facts

--- basic_protein_information.py
+++ basic_protein_information.py
@@ -13,10 +13,18 @@ def get_protein_facts (name):
         img_ind = stripped.find("Images for kids", loop_ind)
         if i != img_count:
             loop_ind = img_ind + 15
+    ad_count = stripped.count('"adsbygoogle"')
+    ad_ind = 0
+    for i in range(0, ad_count):
+        if i != ad_count:
+            ad_ind = stripped.find('<ins class="adsbygoogle"')
+            ad_end = stripped.find("push({});", ad_ind) + 9
+            stripped = stripped.replace(stripped[ad_ind:ad_end], "")
     facts = stripped[start_ind:stripped.find("Images for kids", loop_ind)]
     facts = facts[0:facts.find("Related pages", 0)]
     return facts;
 
-print(get_protein_facts('myosin'))
+print(get_protein_facts('hemoglobin'))
+
 
 #need to loop through count of images and set index to after the second to last image
